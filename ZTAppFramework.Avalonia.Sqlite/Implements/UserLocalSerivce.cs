0dcfce98670e22600ac0d9508b0bf31f8daa70ff
10335 <10335@ZT1995>
2023-02-25 15:44:22 +0800

    更细框架结构

---
+++ ZTAppFramework.Avalonia.Sqlite/Implements/UserLocalSerivce.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZTAppFramework.Sqlite.Models;
+using ZTAppFramework.Sqlite.Repository;
+
+namespace ZTAppFramework.Sqlite.Implements
+{
+    /// <summary>
+    ///********************************************
+    /// 创建人        ：  ZT
+    /// 创建时间      ：  2022/9/5 9:46:14 
+    /// Description   ：  
+    ///********************************************/
+    /// </summary>
+    public class UserLocalSerivce:BaseService<Account>
+    {
+
+    }
+}
---
+++ ZTAppFramework.Avalonia.Sqlite/Utils/AppKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTAppFramework.Avalonia.Sqlite.Utils
+{
+    public static class AppKeys
+    {
+        public const string SaveUserInfoKey = "SaveUserInfo";
+    }
+}
