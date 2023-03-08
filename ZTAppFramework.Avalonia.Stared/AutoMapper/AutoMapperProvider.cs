78bd0059bca637fc633dba2f74cb1cb88b6d9d46
10335 <10335@ZT1995>
2023-03-08 16:48:52 +0800

    更新autoMapper 验证

---
+++ ZTAppFramework.Avalonia.Stared/AutoMapper/AutoMapperProvider.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using AutoMapper.Configuration;
+using AutoMapper.Execution;
+using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZTAppFramework.Avalonia.Stared.AutoMapper
+{
+    public interface IAutoMapperProvider
+    {
+        IMapper GetMapper();
+        void Addprofile(params Profile[] profile);
+    }
+
+    public class AutoMapperProvider : IAutoMapperProvider
+    {
+        private readonly IContainerProvider _container;
+        private MapperConfiguration _configuration;
+
+
+
+        public AutoMapperProvider(IContainerProvider container)
+        {
+            _container = container;
+
+        }
+
+        public void Addprofile(params Profile[] profile)
+        {
+            _configuration = new MapperConfiguration(configure =>
+            {
+                configure.ConstructServicesUsing(_container.Resolve);
+
+                //扫描profile文件
+                configure.AddProfiles(profile);
+            });
+        }
+
+        public IMapper GetMapper()
+        {
+            return _configuration.CreateMapper();
+        }
+    }
+}
